bac0cb846f62a475079d99e24c215d1ce7814327
SimonMarkWhittle <smw2ed@virginia.edu>
2019-04-08 16:24:30 -0400

    fixed lookat script

--- JapaneseEscapeRoom/Assets/Scripts/LookAt.cs
+++ JapaneseEscapeRoom/Assets/Scripts/LookAt.cs
@@ -1,21 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 
 public class LookAt : MonoBehaviour
 {
-    public GameObject target;
+    Transform target;
+    Player player;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = Player.instance;
+        target = player.hmdTransform;
     }
 
     // Update is called once per frame
     void Update()
     {
         //transform.LookAt(target.transform);
-        transform.LookAt(2 * transform.position - target.transform.position);
+        transform.LookAt(2 * transform.position - target.position);
     }
 }

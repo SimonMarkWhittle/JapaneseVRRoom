6c363b20b24c1d2d0044af9511e469d535c2a152
SimonMarkWhittle <smw2ed@virginia.edu>
2019-04-15 17:48:18 -0400

    key box
    added key box and buttons with emojis

---
+++ JapaneseEscapeRoom/Assets/FakeButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FakeButton : MonoBehaviour
+{
+    public TextMeshPro textMesh;
+    public ButtonEvents button;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (button != null) {
+            button.onButtonUp += Batsu;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Batsu() {
+        textMesh.text = "X";
+    }
+}
---
+++ JapaneseEscapeRoom/Assets/KeyBox.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyBox : MonoBehaviour
+{
+
+    public ButtonEvents button;
+
+    Animator animator;
+    int isOpen_hash;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isOpen_hash = Animator.StringToHash("isOpen");
+
+        if (button != null) {
+            button.onButtonUp += Open;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Open() {
+        animator.SetBool(isOpen_hash, true);
+    }
+}

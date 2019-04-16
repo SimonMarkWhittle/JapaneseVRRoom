e812759fed1eea144c7b1d5513f5176635641ddf
SimonMarkWhittle <smw2ed@virginia.edu>
2019-04-16 12:22:58 -0400

    Bonfire and all that good stuff

---
+++ JapaneseEscapeRoom/Assets/Scripts/Bonfire.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bonfire : MonoBehaviour
+{
+    public List<GameObject> fires;
+
+    AreaTrigger trigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject fire in fires) {
+            fire.SetActive(false);
+        }
+
+        trigger = GetComponentInChildren<AreaTrigger>();
+
+        if (trigger != null)
+            trigger.triggerEvent += GetLit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GetLit() {
+        Debug.Log("Get Lit Fam");
+        foreach (GameObject fire in fires) {
+            fire.SetActive(true);
+        }
+    }
+}
